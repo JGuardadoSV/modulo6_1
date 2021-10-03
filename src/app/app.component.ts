56cc9301120d1d24618193215bd06e0442847d3a
JGuardadoSV <47751227+JGuardadoSV@users.noreply.github.com>
2021-10-02 20:57:08 -0600

    mapa con un marcador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,32 @@ import { environment } from 'src/environments/environment';
 })
 export class AppComponent implements OnInit{
 
-  mapa= Mapboxgl.Map;
+  mapa:Mapboxgl.Map;
   title = 'proyectomapa';
 
   ngOnInit(){
 
     (Mapboxgl as any).accessToken = environment.tokenmapa;
-    var map = new Mapboxgl.Map({
+   this.mapa = new Mapboxgl.Map({
     container: 'contenedormapa',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center:[-88.94,14.04 ],
+    zoom:16.6
     });
 
+    this.marcador(-88.93968893127821,14.040098665086017);
+
+  }
+
+
+  marcador(lon:number,lat:number){
+    const marca=new Mapboxgl.Marker({
+      draggable:true
+    })
+    .setLngLat([lon,lat])
+    .addTo(this.mapa);
+
+    marca.on;
   }
 
 }
