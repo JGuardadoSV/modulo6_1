2971d0eec94b417cc098cbe45c1033d77c9600ac
JGuardadoSV <47751227+JGuardadoSV@users.noreply.github.com>
2021-10-02 20:24:30 -0600

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as Mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  mapa= Mapboxgl.Map;
   title = 'proyectomapa';
+
+  ngOnInit(){
+
+    (Mapboxgl as any).accessToken = environment.tokenmapa;
+    var map = new Mapboxgl.Map({
+    container: 'contenedormapa',
+    style: 'mapbox://styles/mapbox/streets-v11'
+    });
+
+  }
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  tokenmapa:'pk.eyJ1Ijoiamd1YXJkYWRvIiwiYSI6ImNrdWFpZWpnaDBoMG0ydm5ucTJnb2d0YjgifQ.e-e6NrNZPzJYFJ9zZT5sLA'
 };
+
+
